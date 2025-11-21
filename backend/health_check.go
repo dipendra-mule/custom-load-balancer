1ffdc07b5f078fc3adb58789bb9d04d5e39d589d
dipendra92 <dipendramule88@gmail.com>
2025-11-21 19:59:40 +0530

    healthcheck

--- backend/health_check.go
+++ backend/health_check.go
@@ -1,6 +1,8 @@
 package backend
 
 import (
+	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -16,4 +18,67 @@ type HealthCheckConfig struct {
 	Timeout      time.Duration
 	Path         string
 	SuccessCodes []int
+}
+
+func NewHealthChecker(pool *BackendPool, config HealthCheckConfig) *HealthChecker {
+	return &HealthChecker{
+		pool:     pool,
+		config:   config,
+		stopChan: make(chan bool),
+	}
+}
+
+func (hc *HealthChecker) Start() {
+	ticker := time.NewTicker(hc.config.Interval)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				hc.checkHealth()
+			case <-hc.stopChan:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func (hc *HealthChecker) Stop() {
+	close(hc.stopChan)
+}
+
+func (hc *HealthChecker) checkHealth() {
+	servers := hc.pool.GetServers()
+
+	for _, server := range servers {
+		go func(s *BackendServer) {
+			alive := hc.isBackendAlive(s.URL)
+			hc.pool.MarkBackendStatus(s.URL, alive)
+		}(server)
+	}
+}
+
+func (hc *HealthChecker) isBackendAlive(backendURL *url.URL) bool {
+	client := http.Client{
+		Timeout: hc.config.Timeout,
+	}
+
+	healthURL := *backendURL
+	healthURL.Path = hc.config.Path
+
+	resp, err := client.Get(healthURL.String())
+	if err != nil {
+		return false
+	}
+	defer resp.Body.Close()
+
+	// Check if status code is in success codes
+	for _, code := range hc.config.SuccessCodes {
+		if resp.StatusCode == code {
+			return true
+		}
+	}
+
+	return false
 }
